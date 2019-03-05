9136a3d7c3130512979bf7a9252169e9fb4f4c4c
Ahmed Nageeb <amn951753@gmail.com>
2019-03-04 23:43:45 -0800

    Update main.js

--- login/main.js
+++ login/main.js
@@ -2,6 +2,6 @@
 function myFunction() {
     var inp = document.getElementById("inp");
     var inp2 = document.getElementById("inp2");
-    if(inp.value === "user@ahmednageeb.com" && inp2.value === "user@google.com"){
+    if(inp.value === "" && inp2.value === ""){
       location.href = './1.pdf'
     }}
