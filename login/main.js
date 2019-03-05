dd7a7b421593f8aaaa1aec9deb8d37fcbe0c2fdf
Ahmed Nageeb <amn951753@gmail.com>
2019-03-04 23:42:37 -0800

    My Site

---
+++ login/main.js
@@ -0,0 +1,7 @@
+
+function myFunction() {
+    var inp = document.getElementById("inp");
+    var inp2 = document.getElementById("inp2");
+    if(inp.value === "user@ahmednageeb.com" && inp2.value === "user@google.com"){
+      location.href = './1.pdf'
+    }}
